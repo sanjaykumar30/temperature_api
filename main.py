c2741a90cc816eb9f28d2d65a3a359ac7d569b86
sanjaykumar30 <m.sanjaykumar3072002@gamil.com>
2022-04-29 00:18:22 +0530

    initial commit

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ app=FastAPI()
 @app.get("/temperature/{tem}")
 def update_db(tem):
     sql="INSERT INTO tem_data(temperature_celcius) VALUES("+str(tem)+")"
-    # print (sql)
     runquery(sql)
     return None
